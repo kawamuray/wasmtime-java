7a88bb726cffea969aef216a90661f11adeeb004
Yuta Saito <kateinoigakukun@gmail.com>
2021-05-31 02:23:58 +0900

    Add test cases for WasiConfig constructors

---
+++ src/test/java/io/github/kawamuray/wasmtime/wasi/WasiConfigTest.java
@@ -0,0 +1,38 @@
+package io.github.kawamuray.wasmtime.wasi;
+import io.github.kawamuray.wasmtime.Store;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Optional;
+import java.io.File;
+import java.io.IOException;
+
+public class WasiConfigTest {
+    @Test
+    public void testNewConfig() {
+        WasiConfig config = new WasiConfig(new String[0], new WasiConfig.PreopenDir[0]);
+        try (Store store = new Store();
+            Wasi wasi = new Wasi(store, config)
+        ) {}
+    }
+
+    @Test
+    public void testNewConfigWithArgs() {
+        WasiConfig config = new WasiConfig(new String[]{"foo", "bar"}, new WasiConfig.PreopenDir[0]);
+        try (Store store = new Store();
+            Wasi wasi = new Wasi(store, config)
+        ) {}
+    }
+
+    @Test
+    public void testNewConfigWithStdOutput() throws IOException {
+        File f = File.createTempFile("wasmtime-jni-wasi-config", "");
+        WasiConfig config = new WasiConfig(
+            new String[0], new WasiConfig.PreopenDir[0],
+            Optional.of(f.getPath()), Optional.of(f.getPath())
+        );
+        try (Store store = new Store();
+            Wasi wasi = new Wasi(store, config)
+        ) {}
+        f.delete();
+    }
+}
