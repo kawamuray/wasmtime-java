8c1f745677639ca17124bc811909e43d706ce5c8
Lucas BENARD <lbenard@student.42.fr>
2021-12-18 01:18:18 +0100

    Fixed wrong var name

--- src/main/java/io/github/kawamuray/wasmtime/InterruptHandle.java
+++ src/main/java/io/github/kawamuray/wasmtime/InterruptHandle.java
@@ -3,8 +3,8 @@ package io.github.kawamuray.wasmtime;
 public class InterruptHandle {
     private final long innerPtr;
 
-    public InterruptHandle(long storePtr) {
-        innerPtr = storePtr;
+    public InterruptHandle(long interruptHandlerPtr) {
+        innerPtr = interruptHandlerPtr;
     }
 
     public native void interrupt();
